ea497c834143dcb0e6169bf7595fbb79957353f9
James Porter <43405677+JamesP1996@users.noreply.github.com>
2020-02-09 18:24:39 +0000

    Fixed Naming Convention as previous could be confusing.

--- MJPhonesProject/asgi.py
+++ MJPhonesProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MJPhones project.
+ASGI config for MJPhonesProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MJPhones.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MJPhonesProject.settings')
 
 application = get_asgi_application()
