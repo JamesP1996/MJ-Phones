bc6d1fb06747ae7850ded996f76a7c4b44308933
James Porter <43405677+jamesp1996@users.noreply.github.com>
2020-02-09 19:57:57 +0000

    Setup Basic Nav-bar - James

--- MJPhones/settings.py
+++ MJPhones/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Phones.apps.PhonesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -114,7 +115,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
--- Phones/views.py
+++ Phones/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+phones = [
+    {
+        'brand': 'Samsung',
+        'name': 'Galaxy S10',
+        'screen': '1920x1080'
+    }
+]
+
 
 # Create your views here.
+def home(request):
+    context = {
+        'phones': phones
+    }
+    return render(request, 'phone/home.html', context)
+
+def about(request):
+    return render(request, 'phone/about.html', {'title': 'About'})
