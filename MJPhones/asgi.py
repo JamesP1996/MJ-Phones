abb36bc66f1f31ad5a23fef057f3574972209a01
James Porter <43405677+JamesP1996@users.noreply.github.com>
2020-02-09 18:33:20 +0000

    Revert "Fixed Naming Convention as previous could be confusing."
    This reverts commit ea497c83

--- MJPhones/asgi.py
+++ MJPhones/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MJPhonesProject project.
+ASGI config for MJPhones project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MJPhonesProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MJPhones.settings')
 
 application = get_asgi_application()
